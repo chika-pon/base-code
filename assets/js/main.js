503983c5f9a7491d3451993654bbea3e83f7ca66
chikapon <chikacode.xxx@gmail.com>
2025-09-03 00:48:03 +0900

    250903

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,3 +103,113 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+// 背景固定＋奥行き感スクロール
+document.addEventListener('DOMContentLoaded', function () {
+  const orImg = document.getElementById('or-img');
+  const cpontan = document.getElementById('cpontan');
+
+  // 2秒後: モノクロ化
+  setTimeout(() => {
+    orImg.classList.add('gray');
+
+    // さらに2秒後: フェードアウト
+    setTimeout(() => {
+      orImg.classList.add('fadeout');
+
+      // さらに2秒後: C-PONTAN スライドイン
+      setTimeout(() => {
+        cpontan.classList.add('animate');
+      }, 2000);
+
+    }, 2000);
+  }, 2000);
+});
+
+
+
+// scroll animation
+document.querySelectorAll(".scroll_wrap").forEach((wrap) => {
+  const triggers = wrap.querySelectorAll(".scroll_trigger");
+  const items = wrap.querySelectorAll(".scroll_item");
+
+  triggers.forEach((trigger, index) => {
+    const background = trigger.querySelector(".scroll_background");
+    const item = items[index];
+
+    if (index === 0) {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: trigger,
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+          markers: true,
+        },
+        defaults: { ease: "none" },
+      });
+
+      tl.fromTo(
+        item,
+        { clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)" },
+        { clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)" }
+      );
+    } else if (index === items.length - 1) {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: trigger,
+          start: "top bottom",
+          end: "bottom bottom",
+          scrub: true,
+          // markers: true,
+        },
+        defaults: {
+          ease: "none"
+        },
+      });
+
+      tl.fromTo(
+        item,
+        { clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)" },
+        { clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)" }
+      );
+    } else {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: trigger,
+          start: "top bottom",
+          end: "bottom top",
+          scrub: true,
+          // markers: true,
+        },
+        defaults: {
+          ease: "none"
+        },
+      });
+
+      tl.fromTo(
+        item,
+        { clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)" },
+        { clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)" }
+      );
+      tl.to(
+        item, {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+      });
+    }
+
+    const tl2 = gsap.timeline({
+      scrollTrigger: {
+        trigger: trigger,
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+      },
+      defaults: {
+        ease: "none"
+      }
+    });
+    tl2.to(background, { yPercent: 50 });
+  });
+});
+
